45b0c81fd9cf2c04a08d9b8b4458638052405900
waitaction <350561378@qq.com>
2016-02-27 23:36:35 +0800

    小改动

--- RESTfulFramework.NET.Business/select.cs
+++ RESTfulFramework.NET.Business/select.cs
@@ -7,21 +7,7 @@ namespace RESTfulFramework.NET.Business
         public ResponseModel RunApi(RequestModel source)
         {
             return null;
-            //try
-            //{
-            //    var jsonEntity = new JsonEntityToSql();
-            //    var sql = jsonEntity.Json2SelectSql((string)source.Tag);
-            //    var dbHelper = Factory.GetInstance<IDBHelper>();
-            //    var dbResult = dbHelper.QuerySql<object>(sql);
-            //    if (dbResult != null)
-            //        return new ResponseModel { Code = Code.Sucess, Msg = dbResult };
-            //    else
-            //        return new ResponseModel { Code = Code.Fail, Msg = "没有数据" };
-            //}
-            //catch (Exception ex)
-            //{
-            //    return new ResponseModel { Code = Code.SystemException, Msg = ex.Message };
-            //}
+          
         }
     }
 }
---
+++ RESTfulFramework.NET.Service/Program.cs
@@ -0,0 +1,44 @@
+﻿using RESTfulFramework.NET.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using RESTfulFramework.NET.DataService;
+namespace RESTfulFramework.NET.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //检测配置文件数据库配置
+            //检测redis配置
+            //
+            var wHost = new System.ServiceModel.Web.WebServiceHost(typeof(RESTfulFramework.NET.DataService.DataService), new Uri(ConfigurationManager.AppSettings["baseUrl"]));
+            var httpBinding = new WebHttpBinding();
+            wHost.AddServiceEndpoint(typeof(IService), httpBinding, ConfigurationManager.AppSettings["relativeUrl"]);
+
+            if (wHost.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
+            {
+                //行为 
+                var behavior = new ServiceMetadataBehavior { HttpGetEnabled = true };
+                //元数据地址 
+                wHost.Description.Behaviors.Add(behavior);
+            }
+
+            //启动 
+            if (wHost.State != CommunicationState.Opened)
+            {
+                wHost.Open();
+                Console.WriteLine("已启动控制台RESTful服务。");
+            }
+            else {
+                Console.WriteLine("启动控制台RESTful服务失败。");
+            }
+            System.Console.ReadKey();
+            wHost.Close();
+        }
+    }
+}
