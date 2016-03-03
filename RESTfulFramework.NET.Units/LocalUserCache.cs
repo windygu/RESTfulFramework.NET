9748062fec3fe2051090ec0a994373de9ac081d7
waitaction <350561378@qq.com>
2016-03-03 23:31:04 +0800

    添加了sqlite数据库的支持及支持字典保存用户状态

---
+++ RESTfulFramework.NET.Units/LocalUserCache.cs
@@ -0,0 +1,54 @@
+﻿using PluginPackage;
+using RESTfulFramework.NET.ComponentModel;
+using RESTfulFramework.NET.Units.Model;
+using System;
+using System.Collections.Generic;
+
+namespace RESTfulFramework.NET.Units
+{
+    public class LocalUserCache : IUserCache<UserInfo>
+    {
+
+        public LocalUserCache() { }
+        private static Dictionary<string, UserInfo> Client { get; set; }
+
+        static LocalUserCache()
+        {
+            //所有用户基本信息缓存redis
+            var users = Factory.GetInstance<IDBHelper>().QuerySql<List<Dictionary<string, object>>>($"SELECT * FROM `user`;");
+            foreach (var user in users)
+            {
+                var redisuser = new UserInfo
+                {
+                    account_name = user["account_name"].ToString(),
+                    account_type_id = user["account_type"].ToString(),
+                    create_time = Convert.ToDateTime(user["create_time"].ToString()).ToString("yyyy-MM-dd HH:mm:ss"),
+                    id = Guid.Parse(user["id"].ToString()),
+                    passwrod = user["passwrod"].ToString(),
+                    real_name = user["realname"].ToString()
+                };
+                Client.Add(redisuser.id.ToString(), redisuser);
+            }
+        }
+        public bool ContainsUserInfo(string token) => Client.ContainsKey(token);
+
+
+        public UserInfo GetUserInfo(string token) => Client.GetValueOrDefault(token);
+
+
+        public bool RemoveUserInfo(string token) => Client.Remove(token);
+
+
+        public bool SetUserInfo(UserInfo userInfo, string token)
+        {
+            if (!ContainsUserInfo(token))
+            {
+                Client.Add(token, userInfo);
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
---
+++ RESTfulFramework.NET.Units/SqliteDBHelper.cs
@@ -0,0 +1,69 @@
+﻿using PluginPackage;
+using RESTfulFramework.NET.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+
+namespace RESTfulFramework.NET.Units
+{
+    public class SqliteDBHelper : IDBHelper
+    {
+        public SqliteDBHelper() { }
+
+        private static IJsonSerialzer JsonSerialzer { get; set; }
+        //private static ILogManager LogManager { get; set; }
+        static SqliteDBHelper()
+        {
+            JsonSerialzer = Factory.GetInstance<IJsonSerialzer>();
+            //LogManager = Factory.GetInstance<ILogManager>();
+        }
+
+
+        public string ConnectionString { get; set; } = ConfigurationManager.ConnectionStrings["RESTfulFrameworkConnection"].ToString();
+
+        public int ExcuteSql(string sql)
+        {
+            var dbconnection = new SQLiteConnection(ConnectionString);
+            var dbcommand = new SQLiteCommand();
+
+            dbcommand.Connection = dbconnection;
+            if (dbconnection.State == ConnectionState.Closed) dbconnection.Open();
+
+            dbcommand.Transaction = dbconnection.BeginTransaction();
+            dbcommand.CommandText = sql;
+
+            try
+            {
+                var i = dbcommand.ExecuteNonQuery();
+                dbcommand.Transaction.Commit();
+                dbconnection.Close();
+                return i;
+            }
+            catch (Exception ex)
+            {
+                dbcommand.Transaction.Rollback();
+                throw ex;
+            }
+        }
+
+        public T QuerySql<T>(string sql) where T : class
+        {
+            var dbconnection = new SQLiteConnection(ConnectionString);
+            var dbcommand = new SQLiteCommand();
+            dbcommand.Connection = dbconnection;
+            var dba = new SQLiteDataAdapter(dbcommand);
+            if (dbconnection.State == ConnectionState.Closed) dbconnection.Open();
+            dbcommand.CommandText = $"{sql};";
+            var dt = new DataTable();
+            dba.Fill(dt);
+            var json = JsonSerialzer.SerializeObject(dt.ToDictionary());
+            dbconnection.Close();
+            return JsonSerialzer.DeserializeObject<T>(json);
+
+        }
+    }
+}
